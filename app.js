f447a6e1c49076c30f42a2557a93d90bd6d366f4
Harsh Nesari <131358039+Harshnesari-210@users.noreply.github.com>
2024-10-16 19:00:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const projectK=()=>{
+    var popup=document.createElement("div")
+
+    popup.style.height="30px";
+    popup.style.width="100px";
+    popup.innerHTML="Some One just signed up"
+}
+
+setTimeout(()=>{
+     popup.style.display="none"
+},5000)
