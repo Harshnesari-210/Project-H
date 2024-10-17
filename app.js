5623da903e8810c60e442d204663b98cfd3feb4a
Harsh Nesari <131358039+Harshnesari-210@users.noreply.github.com>
2024-10-17 12:32:05 +0530

    count app.js

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ async function projectK(){
                 });
 
                 if (response.ok) {
+
+                    const countResponse=await fetch("http://localhost:3000/signupCount",{
+                        methode:"GET",
+                         headers: {
+                    'Content-Type': 'application/json',
+                },
+                    })
+
+                    if(resonse,ok){
+                        const countData=await countResponse.json();
+                        const {count}=countData;
+                    
                     console.log("User signed up successfully!");
                       // Show the popup after successful signup
                     var popup = document.createElement("div");
@@ -28,9 +40,9 @@ async function projectK(){
                     popup.style.position = "fixed";
                     popup.style.top = "10px";
                     popup.style.right = "10px";
-                    popup.innerHTML = "Someone just signed up!";
+                    popup.innerHTML = `${count poplese are signup last 30 minutes ago}`;
 
-                    //document.body.appendChild(popup);
+                    document.body.appendChild(popup);
 
                     setTimeout(() => {
                         popup.style.display = "none";
@@ -38,6 +50,9 @@ async function projectK(){
                 }
      else {
                     console.error("Signup failed!");
+                }
+                    else{
+                    console.log("Count failed");
                 }
             } catch (error) {
                 console.error("Error during signup:", error);
